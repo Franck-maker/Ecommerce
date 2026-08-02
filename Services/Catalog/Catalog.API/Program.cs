6e4b9a590ce898e69b310c250f7220b0ba405a6e
Franck Abdiel <foziefranck@gmail.com>
2026-08-02 09:56:01 +0200

    wiring up components in program.cs file

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,14 +1,42 @@
+using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Repositories;
 using Catalog.Infrastructure.Settings;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Register custom Serializers
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi(); 
+
+//Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//register MediatR
+var assemblies = new Assembly[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof(GetAllBrandsHandler).Assembly
+}; 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+
+//Custom services
+builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ITypeRepository, TypeRepository>();
 
 //Bind strongly-typed settings
 builder.Services.Configure<DatabaseSettings>(
@@ -23,11 +51,22 @@ builder.Services.AddSingleton<IMongoClient>(sp =>
 
 var app = builder.Build();
 
+//Seed Mongo db on startup
+using(var scope = app.Services.CreateScope())
+{
+    var options = scope.ServiceProvider.GetRequiredService<IOptions<DatabaseSettings>>();
+    await DatabaseSeeder.SeedAsync(options);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+//Enable Swagger
+app.UseSwagger();
+app.UseSwaggerUI(); 
 
 app.UseHttpsRedirection();
 
