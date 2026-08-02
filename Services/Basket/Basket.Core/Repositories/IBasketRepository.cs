03a3a82c7dd87b2c6ce7db19eb47bef1635dac18
Franck Abdiel <foziefranck@gmail.com>
2026-08-02 14:17:27 +0200

    creating and implementing Basket Repository

---
+++ Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,17 @@
+﻿using Basket.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Core.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+
+        //Upsert for update or insert a new basket if it doesn't exist
+        Task<ShoppingCart> UpsertBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string userName);
+
+    }
+}
