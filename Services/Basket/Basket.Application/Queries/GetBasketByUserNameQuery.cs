cf6cfc36f889de286e0974abbc9735f24a5ba0f7
Franck Abdiel <foziefranck@gmail.com>
2026-08-04 04:37:25 +0200

    creating delegate base mapper using Func for getting basket and implementing DeleteBasket by UserName command and handler

---
+++ Services/Basket/Basket.Application/Queries/GetBasketByUserNameQuery.cs
@@ -0,0 +1,8 @@
+﻿using Basket.Application.Responses;
+using MediatR;
+
+namespace Basket.Application.Queries
+{
+    public record GetBasketByUserNameQuery(string userName) : IRequest<ShoppingCartResponse>;
+    
+}
