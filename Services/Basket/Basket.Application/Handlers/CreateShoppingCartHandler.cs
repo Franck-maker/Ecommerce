24284bd751e3b2828da044f2b09fc539bd971f08
Franck Abdiel <foziefranck@gmail.com>
2026-08-04 05:04:02 +0200

    creating shoppingCart command and handler

---
+++ Services/Basket/Basket.Application/Handlers/CreateShoppingCartHandler.cs
@@ -0,0 +1,31 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.DTOs;
+using Basket.Application.Mappers;
+using Basket.Application.Responses;
+using Basket.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Application.Handlers
+{
+    public class CreateShoppingCartHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public CreateShoppingCartHandler(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+        public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            // Convert Command to domain entity
+            var shoppingCartEntity = request.ToEntity(); 
+            //Save to Redis
+            var updatedCart = await _basketRepository.UpsertBasket(shoppingCartEntity);
+            //Convert back to response
+            return updatedCart.ToResponse();
+        }
+    }
+}
