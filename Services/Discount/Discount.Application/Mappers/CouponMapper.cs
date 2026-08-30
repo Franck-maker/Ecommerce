7bc9f13a8a59a0e5f2945a1ca840a3b1ee5723fa
Franck Abdiel <foziefranck@gmail.com>
2026-08-30 10:27:07 +0200

    implementing dicount repository, query, handler and mapper

---
+++ Services/Discount/Discount.Application/Mappers/CouponMapper.cs
@@ -0,0 +1,17 @@
+﻿using Discount.Application.DTOs;
+using Discount.Core.Entities;
+
+namespace Discount.Application.Mappers
+{
+    public static class CouponMapper
+    {
+        public static CouponDto ToDto(this Coupon coupon)
+        {
+            return new CouponDto(
+                coupon.Id,
+                coupon.ProductName,
+                coupon.Description,
+                coupon.Amount); 
+        }
+    }
+}
